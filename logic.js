77dadc88296dd28783ae187cd514a4abed610b85
Kumar S <164345496+KuMAr-ShEttY@users.noreply.github.com>
2024-04-06 09:01:22 +0530

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,40 @@
+let btn = document.getElementById("shorten");
+
+btn.addEventListener("click", short);
+
+async function short() {
+  let longURL = document.getElementById("longurl").value;
+  let shortURL = document.getElementById("shorturl");
+
+  const inputBody = JSON.stringify({
+    url: longURL,
+    expiry: "5m",
+  });
+
+  const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    "x-api-key": "sk_e9dec4b7c1194c7b8f68036aca8fc633",
+  };
+
+  try {
+    const result = await fetch("https://api.manyapis.com/v1-create-short-url", {
+      method: "POST",
+      body: inputBody,
+      headers: headers,
+    });
+
+    const data = await result.json();
+
+
+    if (data && data.shortUrl) {
+      shortURL.value = data.shortUrl;
+
+      console.log("Shortened URL:", data.shortUrl);
+    } else {
+      console.error("Unexpected response structure:", data);
+    }
+  } catch (error) {
+    console.error("Error during URL shortening:", error);
+  }
+}
